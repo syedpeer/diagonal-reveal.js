92b06de3a5beab4bc6d29ec6b16a5cf6db78ba92
sophie <April_Sophie@163.com>
2018-08-18 12:17:33 +0800

    reconstruct geometric algorithm

--- README.md
+++ README.md
@@ -3,44 +3,52 @@
 [![code style: prettier](https://img.shields.io/badge/code_style-prettier-ff69b4.svg)](https://github.com/prettier/prettier)
 [![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)](https://github.com/FrontendSophie/diagonal-mask.js/blob/master/LICENSE)
 
-> 📐45 degree reveal animation.
+> a diagonal reveal animation library.
 
 [View demo](http://frontendsophie.com/diagonal-reveal.js/)
 
 ## Usage
 
-1. import `diagonal-reveal.min.js`
-2. add `.diagonal` to target element
-3. set required css style:
-
 ```
-    .diagonal {
-        position: relative;
-        overflow: hidden;
-    }
+<!-- 1. set required css -->
+.diagonal {
+    position: relative;
+    overflow: hidden;
+}
 
-    .diagonal-layer {
-        position: absolute;
-        top: 0;
-        left: 0;
-        transform-origin: top left;
-    }
+.diagonal-layer {
+    position: absolute;
+    top: 0;
+    left: 0;
+    transform-origin: top left;
+}
+
+<!-- 2. add `.diagonal` to target element -->
+<div class="diagonal">
+</div>
+
+<!-- 3. import `diagonal-reveal.min.js` -->
+<script src="./diagonal-reveal.min.js"></script>
+
+<!-- 4. run `diagonalIn()` when needed -->
+<script>
+    diagonalIn()
+</script>
 ```
-
-4. run `diagonalIn()` when needed
 
 ## Options
 
-| key       | description                                                      | default     | type   |
-| :-------- | ---------------------------------------------------------------- | ----------- | ------ |
-| selector  | to specify target, e.g. `.diagonal.first`                        | `.diagonal` | string |
-| direction | four options: `down-left`, `down-right`, `top-left`, `top-right` | `down-left` | string |
+| key       | description                               | default     | type   |
+| :-------- | ----------------------------------------- | ----------- | ------ |
+| selector  | to specify target, e.g. `.diagonal.first` | `.diagonal` | string |
+| direction | animation direction: `left`, `right`      | `left`      | string |
+| degree    | angle of inclination                      | 45          | number |
 
 ## Q & A
 
-Q: 1. How to amend style and animation?  
-A: Add style to `.diagonal-reveal`, do not cover required css set in Usage.  
-For example, when amending animation delay time, set `animation-delay` instead of rewrite `animation`. Meanwhile add `!important` to force cover.
+Q: 1. How to amend layer style and animation?  
+A: Add style to `.diagonal-layer`, do not cover required css set in Usage.  
+For instance, when amending animation delay time, set `animation-delay` instead of rewrite `animation` and add `!important` to force cover.
 
-Q: 2. How to amend reveal direction?  
-A: pass parameter like `diagonalIn({direction: 'down-right'})`.
+Q: 2. How to amend reveal direction and degree?  
+A: pass parameter like `diagonalIn({direction: 'right', degree: 60})`.
--- diagonal-reveal.js
+++ diagonal-reveal.js
@@ -2,17 +2,25 @@
     let diagonalId = 0;
     const defaultConfig = {
         selector: '.diagonal',
-        direction: 'down-left',
+        direction: 'left',
+        degree: 45,
     }
-    let decimals
+    let _config
 
     function _diagonalIn(config = defaultConfig) {
-        const _config = Object.assign({}, defaultConfig, config);
+        _config = Object.assign({}, defaultConfig, config)
         const content = document.querySelectorAll(_config.selector)
+
         content.forEach(function (item, index) {
-            const layer = _initializeLayer(item)
-            layer.style.transform = _calTransform('cover', _config.direction)
-            const styleEle = _generateAnimation(layer, index, _config)
+            const geometric = {
+                cWidth: item.offsetWidth,
+                cHeight: item.offsetHeight,
+                offsetX: parseFloat(item.offsetHeight * getTanDeg(_config.degree).toFixed(2))
+            }
+
+            const layer = _initializeLayer(item, geometric)
+            layer.style.transform = _calTransform(geometric)
+            const styleEle = _generateAnimation(layer, index, geometric)
 
             layer.addEventListener('animationend', function () {
                 item.removeChild(layer)
@@ -21,78 +29,41 @@
         })
     }
 
-    function _initializeLayer(item) {
+    function _initializeLayer(item, geometric) {
         const el = document.createElement('div')
         el.className = 'diagonal-layer'
         item.appendChild(el)
 
-        const cWidth = item.offsetWidth // includes border
-        const cHeight = item.offsetHeight
-        const mSize = (cWidth + cHeight) * Math.sqrt(2) / 2 // calculate square layer size
-        el.style.width = `${mSize}px`
-        el.style.height = `${mSize}px`
-
-        const coverPath = cWidth * Math.sqrt(2) / 2 // shortest path to cover
-        decimals = {
-            cover: coverPath / mSize,
-            downLeft: (mSize - coverPath) / mSize,
-            topRight: (mSize + coverPath) / mSize,
-        }
+        el.style.height = `${geometric.cHeight}px`
+        el.style.width = `${geometric.cWidth + geometric.offsetX}px`
 
         return el
     }
 
-    function _decimalToPCT(decimal) {
-        const val = Math.round(decimal * 100)
-        return `${val}%`
+    function getTanDeg(deg) {
+        const rad = deg * Math.PI / 180
+        return Math.tan(rad)
     }
 
-    function _calTransform(status, direction) {
-        let translateX = 0;
-        let translateY = 0;
-        const {
-            cover,
-            downLeft,
-            topRight
-        } = decimals
+    function _calTransform(geometric, status) {
+        let translateX = `${-geometric.offsetX}px`
+        let {
+            direction,
+            degree,
+        } = _config
 
-        switch (direction) {
-            case 'down-left': // from top right to bottom left
-                if (status === 'cover') {
-                    translateY = _decimalToPCT(-cover)
-                } else if (status === 'out') {
-                    translateY = _decimalToPCT(downLeft)
-                }
-                break
-
-            case 'down-right': // from top left to bottom bottom
-                translateY = _decimalToPCT(-cover)
-                if (status === 'out') {
-                    translateX = '100%'
-                }
-                break
-
-            case 'top-left': // from bottom right to top left
-                translateY = _decimalToPCT(-cover)
-                if (status === 'out') {
-                    translateX = '-100%'
-                }
-                break
-
-            case 'top-right': // from bottom left to top right
-                if (status === 'cover') {
-                    translateY = _decimalToPCT(-cover)
-                } else if (status === 'out') {
-                    translateY = _decimalToPCT(-topRight)
-                }
-                break
+        if (status === 'out') {
+            if (direction === 'left') {
+                translateX = `-${geometric.cWidth + geometric.offsetX * 2}px`
+            } else if (direction === 'right') {
+                translateX = `${geometric.cWidth}px`
+            }
         }
 
-        const translateCSS = `translate(${translateX}, ${translateY})`
-        return `rotate(45deg) ${translateCSS}`
+        return `skew(${degree}deg) translateX(${translateX})`
     }
 
-    function _generateAnimation(el, index, config) {
+    function _generateAnimation(el, index, geometric) {
         const aniName = `diagonal-${diagonalId++}-${index}`
         el.style.animation = `${aniName} 0.5s ease-in forwards`
 
@@ -103,10 +74,10 @@
             `
                 @keyframes ${aniName} {
                     from { 
-                        transform: ${_calTransform('cover', config.direction)}
+                        transform: ${_calTransform(geometric, 'cover')}
                     }
                     to  { 
-                        transform: ${_calTransform('out', config.direction)}
+                        transform: ${_calTransform(geometric, 'out')}
                     }
                 }'
             `
